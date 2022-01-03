fcc15b6c8ce6e1367a48478fa81c4f82417996e0
iachella <iachella@student.chalmers.se>
2022-01-03 16:11:10 +0100

    [PAIR] Change local host to remote
    Co-Authored-By: puerto <puerto@student.chalmers.se>

--- broker/broker.js
+++ broker/broker.js
@@ -2,45 +2,46 @@ const mqtt = require("mqtt");
 const topics = require("./topics");
 const validatorTopic = topics.validatorTopic;
 const handlerTopic = topics.bookingHandlerTopic;
-const localHost = 'mqtt://127.0.0.1'; // Local host
-const remoteHost = ''; // Remote host
+const host = "ws://broker.emqx.io:8083/mqtt"
 
-// Change the value of host to the host in use.
-const host = localHost;
-
-const port = ':1883';
+var clientId =
+  "mqttjs_" +
+  Math.random()
+    .toString(16)
+    .substr(3, 8);
 
 const options = {
     keepalive: 60,
 	protocolId: 'MQTT',
 	protocolVersion: 4,
+    clientId: clientId,
+    username: 'test',
+    password: '12',
 	clean: true,
 	reconnectPeriod: 1000,
 	connectTimeout: 30 * 1000,
 	will: {
-		topic: 'WillMsg',
-		payload: 'Connection Closed abnormally..!',
+		topic: 'WillMsg12',
+		payload: 'request-validator failure',
 		qos: 1,
 		retain: false
 	},
-    hostURL: (host+port)
 }
 
-const client = mqtt.connect(options.hostURL, options);
+const client = mqtt.connect(host, options);
 
 function publish(topic, message) {
-    client.publish(topic, message);
+    client.publish(topic, message, { qos: 1, retain:false });
 }
 
 client.on("connect", function() {
     
+    console.log("Connecting mqtt client");
     function subscribe(topic) {
         client.subscribe(topic);
-        console.log("Subscribed to: " + topic);
+        console.log("Subscribed to: " + topic, { qos: 2 });
     }
     subscribe(handlerTopic);
-
-    publish(validatorTopic, 'Validate this: ...', { qos: 1, retain:false });
 })
 
 client.on('message', function(topic, message) {
@@ -49,5 +50,5 @@ client.on('message', function(topic, message) {
         //send appointment to backend for persisting data
         publish(validatorTopic, message, { qos: 1, retain:false });
     }
-    console.log(message.toString());
+    console.log(JSON.parse(message));
 })
