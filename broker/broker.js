8dc68b773cf52c18ca8f155ef7795869e46ab5bf
Kevinayad <kevinay@student.chalmers.se>
2022-01-05 19:37:47 +0100

    added script

--- broker/broker.js
+++ broker/broker.js
@@ -48,7 +48,43 @@ client.on('message', function(topic, message) {
     if (topic == handlerTopic){
         //TODO: check for availability before next line is executed
         //send appointment to backend for persisting data
+        checkAppointment();
         publish(validatorTopic, message, { qos: 1, retain:false });
     }
     console.log(JSON.parse(message));
 })
+async function checkAppointment(appointment) {
+        var clinicID = appointment.dentistid;
+        var date = new Date(appointment.date);
+        var day = date.getDay();
+        var time = appointment.time;
+        var hours = time.slice(0,2);
+        var minutes = time.slice(3,5);
+        date.setHours(hours,minutes,0);
+        date.setTime(date.getTime() + (1*60*60*1000));
+        var check = false;
+        var clinicName = 'Clinic' + (clinicID);
+        var result = await scheduleCollection.findOne({});
+        var clinic = result[clinicName];
+        const allDays = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+        var daySchedule = clinic[allDays[day]];
+        var slot = daySchedule[time];
+        var slotTime = slot.time;
+        if (date.getTime() == slotTime.getTime() && slot.av == true) {
+            slot.av = false;
+            await scheduleCollection.deleteOne({}, function (err, res) {
+                if (err) {throw err};
+                console.log('First schedule removed');
+            });
+            await scheduleCollection.insertOne(result, function (err, res) {
+                if (err) {throw err};
+                console.log('Second schedule added');
+            });
+            check = true;
+        }
+        if (check) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
---
+++ depend.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+npm install mongoose
+npm install moment
+npm install vue
+npm install mapbox
+npm install mqtt
+#run this script by first typing chmod u+x depend.sh in backend folder then ./depend.sh
+#this script installs all needed dependecies
